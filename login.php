5cd4d76280184c8a1a0aa59e3194fb2518ab7c69
gustavo <gustavo>
2024-03-06 21:13:17 -0300

    Up files

---
+++ login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<header>
+<nav>
+        <ul>
+            <li><a href="index.html">inicio</a></li>
+            <li><a href="historia.html">historia da IA</a></li>
+            <li><a href="machineLearning.html">machine Learning</a></li>
+            <li><a href="deepLearning.html">Deep Learning</a></li>
+            <li><a href="cadastro.php">Sign in</a></li>
+            <li><a href="login.php">Log in</a></li>
+        </ul>
+    </nav>
+</header>   
+<body>
+    <div class="container">
+        <h1>Login</h1>
+        <form action="#" method="post">
+            <label for="Email">Email:</label>
+            <input type="email" id="Email" name="Email" required><br><br>
+
+
+            <label for="password">Senha:</label>
+            <input type="password" id="password" name="password" required><br>
+            <a href="esqueceu_senha.php">Esqueceu a senha?</a></p>
+
+            <input type="submit" value="Entrar">
+        </form>
+        <p>Não tem uma conta?<a href="cadastro.php"> Cadastre-se</a>
+    </div>
+</body>
+
+</html>
